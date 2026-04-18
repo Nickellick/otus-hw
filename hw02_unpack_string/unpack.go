6cdd17c2d1954a16b3f55e29a888029224b0d87d
Nikolai Povarov <nd.povarov@gmail.com>
2026-04-18 19:18:46 +0300

    feat: asterisk task

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -13,8 +13,25 @@ func Unpack(input string) (string, error) {
 	builder := &strings.Builder{}
 	var lastRune rune
 	hasLastRune := false
+	isEscaped := false
 
 	for _, char := range input {
+		if isEscaped {
+			if char != '\\' && !unicode.IsDigit(char) {
+				return "", ErrInvalidString
+			}
+			if hasLastRune {
+				builder.WriteRune(lastRune)
+			}
+			lastRune = char
+			hasLastRune = true
+			isEscaped = false
+			continue
+		}
+		if char == '\\' {
+			isEscaped = true
+			continue
+		}
 		if !unicode.IsDigit(char) {
 			if hasLastRune {
 				builder.WriteRune(lastRune)
@@ -35,6 +52,11 @@ func Unpack(input string) (string, error) {
 		}
 		hasLastRune = false
 	}
+
+	if isEscaped {
+		return "", ErrInvalidString
+	}
+
 	if hasLastRune {
 		builder.WriteRune(lastRune)
 	}
