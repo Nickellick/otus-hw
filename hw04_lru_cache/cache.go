fca2262159b4a43d81afa6bee3a4683e1201dc51
Nikolai Povarov <nd.povarov@gmail.com>
2026-05-03 23:17:04 +0300

    feat: test ok, add lru cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,53 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+type cacheItem struct {
+	Key   Key
+	Value interface{}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+
+	if item, ok := c.items[key]; ok {
+		item.Value = cacheItem{
+			Key:   key,
+			Value: value,
+		}
+		c.queue.MoveToFront(item)
+		return true
+	}
+	item := c.queue.PushFront(cacheItem{
+		Key:   key,
+		Value: value,
+	})
+	c.items[key] = item
+	if c.queue.Len() > c.capacity {
+		oldest := c.queue.Back()
+		c.queue.Remove(oldest)
+		oldestItem := oldest.Value.(cacheItem)
+		delete(c.items, oldestItem.Key)
+	}
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	item, ok := c.items[key]
+	if !ok {
+		return nil, false
+	}
+	c.queue.MoveToFront(item)
+	insideValue := item.Value.(cacheItem)
+	return insideValue.Value, true
+}
+
+func (c *lruCache) Clear() {
+	c.items = make(map[Key]*ListItem, c.capacity)
+	c.queue = NewList()
 }
 
 func NewCache(capacity int) Cache {
