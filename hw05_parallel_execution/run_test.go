c733692319bc2f89bbb14e53765e90af34086779
Nikolai Povarov <nd.povarov@gmail.com>
2026-05-16 15:12:00 +0300

    tests: redone tests with "Eventually"
    note: Why time.sleep in first test is needed?

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -3,7 +3,6 @@ package hw05parallelexecution
 import (
 	"errors"
 	"fmt"
-	"math/rand"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -24,7 +23,6 @@ func TestRun(t *testing.T) {
 		for i := 0; i < tasksCount; i++ {
 			err := fmt.Errorf("error from task %d", i)
 			tasks = append(tasks, func() error {
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
 				atomic.AddInt32(&runTasksCount, 1)
 				return err
 			})
@@ -32,26 +30,31 @@ func TestRun(t *testing.T) {
 
 		workersCount := 10
 		maxErrorsCount := 23
+		maxTotalTasks := workersCount + maxErrorsCount
+
 		err := Run(tasks, workersCount, maxErrorsCount)
 
 		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
-		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
+		require.LessOrEqual(t, runTasksCount, int32(maxTotalTasks), "extra tasks were started")
 	})
 
 	t.Run("tasks without errors", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
 
-		var runTasksCount int32
-		var sumTime time.Duration
+		var startedTaskCount int32
+		var finishedTasksCount int32
+
+		eventuallyWaitFor := time.Second
+		eventuallyTick := time.Millisecond * 10
+
+		taskLocker := make(chan struct{})
 
 		for i := 0; i < tasksCount; i++ {
-			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
-			sumTime += taskSleep
-
 			tasks = append(tasks, func() error {
-				time.Sleep(taskSleep)
-				atomic.AddInt32(&runTasksCount, 1)
+				atomic.AddInt32(&startedTaskCount, 1)
+				<-taskLocker
+				atomic.AddInt32(&finishedTasksCount, 1)
 				return nil
 			})
 		}
@@ -59,12 +62,20 @@ func TestRun(t *testing.T) {
 		workersCount := 5
 		maxErrorsCount := 1
 
-		start := time.Now()
-		err := Run(tasks, workersCount, maxErrorsCount)
-		elapsedTime := time.Since(start)
-		require.NoError(t, err)
+		done := make(chan error)
 
-		require.Equal(t, int32(tasksCount), runTasksCount, "not all tasks were completed")
-		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+		go func() {
+			done <- Run(tasks, workersCount, maxErrorsCount)
+		}()
+
+		require.Eventually(t, func() bool {
+			return atomic.LoadInt32(&startedTaskCount) == int32(workersCount)
+		}, eventuallyWaitFor, eventuallyTick, "not all tasks were started in time")
+
+		close(taskLocker)
+		err := <-done
+
+		require.NoError(t, err)
+		require.Equal(t, int32(tasksCount), atomic.LoadInt32(&finishedTasksCount), "not all tasks were completed")
 	})
 }
