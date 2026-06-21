e3d19f227ec445ac3ef88e5e8ab5473f0c351d98
Nikolai Povarov <nd.povarov@gmail.com>
2026-06-21 23:28:38 +0300

    refactor: move copy to separate function

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"os"
 )
 
@@ -18,76 +17,10 @@ func main() {
 	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
 	flag.Int64Var(&offset, "offset", 0, "offset in input file")
 	flag.Parse()
-
-	if from == "" {
-		fmt.Fprintln(os.Stderr, "-from is required")
-		os.Exit(1)
-	}
-
-	if to == "" {
-		fmt.Fprintln(os.Stderr, "-to is required")
-		os.Exit(1)
-	}
-
-	if offset < 0 {
-		fmt.Fprintln(os.Stderr, "-offset must be >= 0")
-		os.Exit(1)
-	}
-
-	if limit < 0 {
-		fmt.Fprintln(os.Stderr, "-limit must be >= 0")
-		os.Exit(1)
-	}
-
-	src, err := os.Open(from)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "open source:", err)
-		os.Exit(1)
-	}
-	defer src.Close()
-
-	dst, err := os.Create(to)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "create destination:", err)
-		os.Exit(1)
-	}
-	defer dst.Close()
-
-	_, err = src.Seek(offset, io.SeekStart)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "seek source:", err)
-		os.Exit(1)
-	}
-
-	var reader io.Reader = src
-
-	info, err := src.Stat()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "stat source:", err)
-		os.Exit(1)
-	}
-	available := info.Size() - offset
-	if available < 0 {
-		available = 0
-	}
-	total := available
-	if limit > 0 {
-		if limit < available {
-			total = limit
-		}
-		reader = io.LimitReader(src, limit)
-	}
-
-	progressReader := &ProgressReader{
-		r:     reader,
-		total: total,
-	}
-
-	written, err := io.Copy(dst, progressReader)
+	err := Copy(from, to, offset, limit)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "copy:", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("copied %d bytes\n", written)
 }
